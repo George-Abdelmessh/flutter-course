52bc4ba86fb59a4da5a515c477d11a235990b93a
George Abd-Elmesseh <76843136+George-Abdelmessh@users.noreply.github.com>
2024-02-28 20:22:32 +0200

    errors

--- main.dart
+++ main.dart
@@ -17,24 +17,41 @@
 add function, to create var x it = 5
  then x plus 1 
 */
-void add() {
-  var x = 5;
-  ++x;
-}
 
-void main () {
-  /// [name]
-  var name;
-  var age = 15; 
-  name = "george";
-  // george
-  print(name);
+// void add() {
+//   var x = 5;
+//   ++x;
+// }
 
-  name = "ahmed";
-  // ahmed
-  print(name);
 
-  age = 23;
-  // 23 hehle snkshgifudhsb iu
-  print(age);
+// void main () {
+//   /// [name]
+//   var name;
+//   var age = 15; 
+//   name = "george";
+//   // george
+//   print(name);
+
+//   name = "ahmed";
+//   // ahmed
+//   print(name);
+
+//   age = 23;
+//   // 23 hehle snkshgifudhsb iu
+//   print(age);
+// }
+
+int X = 10;
+
+void main() {
+  var b = 5 + 5;
+  // 10
+  var x = 10;
+  // 20
+  b = x + b;
+  print(b);
+  //28
+  b = 8 + b;
+  var y = 'x'  + 5;
+  print(y);
 }
