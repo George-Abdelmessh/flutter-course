f749080eef6e22d32c5c8bb7201c7ecc058496e3
George Abd-Elmesseh <76843136+George-Abdelmessh@users.noreply.github.com>
2024-02-28 19:11:48 +0200

    init

---
+++ main.dart
@@ -0,0 +1,15 @@
+// Compiler vs interpreter
+// Syntax
+// main function ** Entry point
+// semicolon 
+// Output
+// var valible
+// basic data types [null, String, int, double, bool]
+// user input stdin.readLineSync()
+// import
+// Operators[Arithmetic(+ - * / ++ -- %), Assignment(= += -= *= ), Comparison(== != > < >= <=), Logical(& | !)]
+
+
+void main () {
+  print('Hello World, My n is George ');
+}
