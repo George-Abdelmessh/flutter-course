de245c0650c12f24616ec59c5a6437584cf35408
George Abd-Elmesseh <76843136+George-Abdelmessh@users.noreply.github.com>
2024-02-28 20:42:15 +0200

    basic data types

--- main.dart
+++ main.dart
@@ -41,17 +41,37 @@ add function, to create var x it = 5
 //   print(age);
 // }
 
-int X = 10;
+// int X = 10;
 
-void main() {
-  var b = 5 + 5;
-  // 10
-  var x = 10;
-  // 20
-  b = x + b;
-  print(b);
-  //28
-  b = 8 + b;
-  var y = 'x'  + 5;
-  print(y);
-}
+// void main() {
+//   var b = 5 + 5;
+//   // 10
+//   var x = 10;
+//   // 20
+//   b = x + b;
+//   print(b);
+//   //28
+//   b = 8 + b;
+//   var y = 'x'  + 5;
+//   print(y);
+// }
+
+// 5 m
+// var => 2m
+
+/// null => 1m  no value
+/// String => 2m text
+/// int => 1m numbers
+/// double => 1.5m numbers floated point 1.5 2.5
+/// bool => 0.5 m true or flase
+/// 
+// void main() {
+//   int age = 5;
+//   String name = "george";
+//   double height = 180.5;
+//   bool maried = false;
+//   bool isMale = true;
+  
+//   print(age.runtimeType);
+// }
+
