c393ef3b2147150efc760bb72bd79eb29759f77c
George Abd-Elmesseh <76843136+George-Abdelmessh@users.noreply.github.com>
2024-02-28 19:54:14 +0200

    comments

--- main.dart
+++ main.dart
@@ -4,12 +4,37 @@
 // semicolon 
 // Output
 // var valible
+// comment
+// Errors
 // basic data types [null, String, int, double, bool]
 // user input stdin.readLineSync()
 // import
 // Operators[Arithmetic(+ - * / ++ -- %), Assignment(= += -= *= ), Comparison(== != > < >= <=), Logical(& | !)]
 
 
+
+/*
+add function, to create var x it = 5
+ then x plus 1 
+*/
+void add() {
+  var x = 5;
+  ++x;
+}
+
 void main () {
-  print('Hello World, My n is George ');
+  /// [name]
+  var name;
+  var age = 15; 
+  name = "george";
+  // george
+  print(name);
+
+  name = "ahmed";
+  // ahmed
+  print(name);
+
+  age = 23;
+  // 23 hehle snkshgifudhsb iu
+  print(age);
 }
